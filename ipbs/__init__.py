5633322fd16e170edbe554d243c2d6c6cb22a64e
Nicholas Bollweg (Nick) <nick.bollweg@gmail.com>
2014-10-25 23:27:19 -0400

    2 vs 3 syntax flake

--- ipbs/__init__.py
+++ ipbs/__init__.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-
 __author__ = 'Nicholas Bollweg'
 __email__ = 'nick.bollweg@gmail.com'
 __version__ = '0.1.0'
--- ipbs/enum/enum3.py
+++ ipbs/enum/enum3.py
@@ -1,3 +1,4 @@
+# flake8: noqa
 def enum(**enums):
     """
     Make an iterable enumeration
